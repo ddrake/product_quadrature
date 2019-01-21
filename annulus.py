f8ebd667d26f961be9e66db1db81527cbb4d587c
Dow Drake <dowdrake@msn.com>
2019-01-20 18:43:25 -0800

    points look good, but weights are still wrong

--- annulus.py
+++ annulus.py
@@ -28,12 +28,12 @@ def GC1_w(i, M):
     return mp.chop(mp.pi / M,tol=1.0e-30)
 
 
-def get_coeffs(n, r1=0, r2=1, is_pos=True):
+def get_coeffs(n, r1=0, r2=1):
     """ Compute recursively pₙ₊₁(x) = (x-aₙ)pₙ(x) + bₙ pₙ₊₁(x)
         with p₀ = 1, p₁ = x
         a₀ = 0, aₙ = (xpₙ, pₙ)/(pₙ, pₙ),
         bₙ = (pₙ, pₙ)/(pₙ₋₁, pₙ₋₁) where all inner products are
-        weighted by w(x) = gamma^2(x+c), with gamma = (r2-r1)/2.
+        weighted by w(x) = x + c.
         For the annulus, we always have c = (r2 + r1) / (r2 - r1) >= 1.
         When r1 = 0, and r2 = 1, then c = 1, 
         and this then provides an alternative quadrature
@@ -47,7 +47,7 @@ def get_coeffs(n, r1=0, r2=1, is_pos=True):
     pp = []                  # (p,p) inner products
     xpp = []                 # (xp,p) inner products
     p = [mpf(1)]             # p₀ = 1
-    w = c + x if is_pos else c - x  # weight (Jacobian) >= 0 for x in [-1,1]
+    w = c + x                # weight (Jacobian) >= 0 for x in [-1,1]
 
     xpp.append(sym.integrate(w*x*p[-1]*p[-1],(x, mpf(-1), mpf(1))))
     pp.append(sym.integrate(w*p[-1]*p[-1],(x,mpf(-1),mpf(1))))
@@ -85,14 +85,11 @@ def rhos_wts(jacobi):
     wts = [mp.chop(c*c,tol=1.0e-30) for c in ev[0, :]]
     return rhos, wts
 
-def rs(rhos, r1, r2, is_pos=True):
+def rs(rhos, r1, r2):
     """ Given the rho values and the inner and outer radii,
         return the radial points for the quadrature.
     """
-    if is_pos:
-        return [(r2-r1)/2*rho + (r2+r1)/2 for rho in rhos]
-    else:
-        return [(r2-r1)/2*rho - (r2+r1)/2 for rho in rhos]
+    return [(r2-r1)/2*rho + (r2+r1)/2 for rho in rhos]
 
 def rdjust(num):
     """ Ensure that each number has exactly 26 significant digits
@@ -108,10 +105,11 @@ def plot_for(a, b, n, r1, r2):
     J = jacobi(a, b, n)
     rhos, wts = rhos_wts(J)
     r = rs(rhos, r1, r2)
-    r += [-rr for rr in r]
-    tt = [GC1_p(i, 2*n) for i in range(1,2*n+1)]
+    tt = [GC1_p(i, n) for i in range(1,n+1)]
     qq = [mp.sqrt(mp.mpf(1)-t*t) for t in tt]
-    cart = [(r[i]*qq[j], r[i]*tt[j]) for i,j in product(range(2*n), range(2*n))]
+    cart1 = [(r[i]*qq[j], r[i]*tt[j]) for i,j in product(range(n), range(n))]
+    cart2 = [(-r[i]*qq[j],-r[i]*tt[j]) for i,j in product(range(n), range(n))]
+    cart = cart1+cart2
     x,y = zip(*cart)
     th = np.linspace(0,2*np.pi,100)
     xout, yout = r2*np.cos(th), r2*np.sin(th)
@@ -133,13 +131,15 @@ def generate_for(a, b, n, r1, r2):
     r = rs(rhos, r1, r2)
     r += [-rr for rr in r]
     wts += wts[:]
-    tt = [GC1_p(i, 2*n) for i in range(1,2*n+1)]
+    tt = [GC1_p(i, n) for i in range(1,n+1)]
     qq = [mp.sqrt(mp.mpf(1)-t*t) for t in tt]
-    print('disk_pars[%d] = { %s,' % (2*n, rdjust(mp.pi / 2/n)))
+    print('disk_pars[%d] = { %s,' % (n, rdjust(mp.pi / n)))
     print('  {')
-    for i in range(2*n-1):
+    for i in range(n-1):
         print('   { %s, %s, %s, %s },' % (rdjust(r[i]), rdjust(tt[i]), rdjust(qq[i]), rdjust(wts[i])))
-    print('   { %s, %s, %s, %s }' % (rdjust(r[2*n-1]), rdjust(tt[2*n-1]), rdjust(qq[2*n-1]), rdjust(wts[2*n-1])))
+        print('   { %s, %s, %s, %s },' % (rdjust(r[i]), rdjust(-tt[i]), rdjust(-qq[i]), rdjust(wts[i])))
+    print('   { %s, %s, %s, %s },' % (rdjust(r[n-1]), rdjust(tt[n-1]), rdjust(qq[n-1]), rdjust(wts[n-1])))
+    print('   { %s, %s, %s, %s }' % (rdjust(r[n-1]), rdjust(-tt[n-1]), rdjust(-qq[n-1]), rdjust(wts[n-1])))
     print('  }')
     print('};')
 
@@ -164,28 +164,49 @@ def f5(x,y):
     return 1
 
 def integrate(f, n, r1, r2):
-    ap, bp, *rest = get_coeffs(n, r1, r2, is_pos=True)
-    an, bn, *rest = get_coeffs(n, r1, r2, is_pos=False)
-    Jp = jacobi(ap, bp, n)
-    Jn = jacobi(an, bn, n)
-    rhop, wtp = rhos_wts(Jp)
-    rhon, wtn = rhos_wts(Jn)
-    rp = rs(rhop, r1, r2, is_pos=True)
-    rn = rs(rhon, r1, r2, is_pos=False)
+    a, b, *rest = get_coeffs(n, r1, r2)
+    J = jacobi(a, b, n)
+    rho, wt = rhos_wts(J)
+    r = rs(rho, r1, r2)
     tt = [GC1_p(i, n) for i in range(1,n+1)]
     qq = [mp.sqrt(mp.mpf(1)-t*t) for t in tt]
+
     cwt = mp.pi / n
     result = 0
     for i in range(n):
         for j in range(n):
-            print('rp', rp[i])
-            print('rn', rn[i])
-            print('(xp,yp)', rp[i]*qq[j], rp[i]*tt[j])
-            print('(xn,yn)', rn[i]*qq[j], rn[i]*tt[j])
-            print('f(xp,yp)', f(rp[i]*qq[j], rp[i]*tt[j]))
-            print('f(xn,yn)', f(rn[i]*qq[j], rn[i]*tt[j]))
-            print('w1p',wtp[i],'w1n',wtn[i],'w2',cwt)
-            result += wtp[i]*cwt*f(rp[i]*qq[j], rp[i]*tt[j]) \
-                    + wtn[i]*cwt*f(rn[i]*qq[j], rn[i]*tt[j])
+            print('r', r[i])
+            print('(x,y)', r[i]*qq[j], r[i]*tt[j])
+            print('(x1,y1)', -r[i]*qq[j], -r[i]*tt[j])
+            print('f(x,y)', f(r[i]*qq[j], r[i]*tt[j]))
+            print('f(x1,y1)', f(-r[i]*qq[j], -r[i]*tt[j]))
+            print('wt',wt[i],'cwt',cwt)
+            result += wt[i]*cwt*f(r[i]*qq[j], r[i]*tt[j]) \
+                    + wt[i]*cwt*f(-r[i]*qq[j], -r[i]*tt[j])
     return result * (r2-r1) * (r2-r1) / 4
+
+
+def g1(r):
+    return 1
+
+def g2(r):
+    return r
+
+def g3(r):
+    return r*r
+
+def integrate1d(g, n, r1, r2):
+    a, b, *rest = get_coeffs(n, r1, r2)
+    J = jacobi(a, b, n)
+    rho, wt = rhos_wts(J)
+    r = rs(rho, r1, r2)
+    result = 0
+    for i in range(n):
+        for j in range(n):
+            print('r', r[i])
+            print('g(r)', g(r[i]))
+            print('wt',wt[i])
+            result += wt[i]*g(r[i])
+    return result * (r2-r1) * (r2-r1) / 4
+
 
